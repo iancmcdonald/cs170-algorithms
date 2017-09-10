9abb2ec48c94af998393d529f30801910876af96
Ian McDonald <ianmcdonald@Ians-MBP-3.attlocal.net>
2017-09-09 18:44:48 -0700

    updated MergeSortTest

--- test/MergeSortTest.java
+++ test/MergeSortTest.java
@@ -8,33 +8,33 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class MergeSortTest {
     private List<Integer> intList;
+    private List<Integer> intGroundTruth;
     private List<Character> charList;
+    private List<Character> charGroundTruth;
 
     @BeforeEach
     void setUp() {
         intList = Arrays.asList(5, 0, 1, 7, 6);
+        intGroundTruth = Arrays.asList(0, 1, 5, 6, 7);
         charList = Arrays.asList('a', 'z', 'd', 'f', 'h', 'c');
+        charGroundTruth = Arrays.asList('a', 'c', 'd', 'f', 'h', 'z');
     }
 
     @Test
     void mergeSort() {
         List<Integer> intResult = MergeSort.mergeSort(intList);
-        List<Integer> intGroundTruth = Arrays.asList(0, 1, 5, 6, 7);
         assertEquals(intResult, intGroundTruth);
 
         List<Character> charResult = MergeSort.mergeSort(charList);
-        List<Character> charGroundTruth = Arrays.asList('a', 'c', 'd', 'f', 'h', 'z');
         assertEquals(charResult, charGroundTruth);
     }
 
     @Test
     void mergeSortIterative() {
         List<Integer> intResult = MergeSort.mergeSortIterative(intList);
-        List<Integer> intGroundTruth = Arrays.asList(0, 1, 5, 6, 7);
         assertEquals(intResult, intGroundTruth);
 
         List<Character> charResult = MergeSort.mergeSortIterative(charList);
-        List<Character> charGroundTruth = Arrays.asList('a', 'c', 'd', 'f', 'h', 'z');
         assertEquals(charResult, charGroundTruth);
     }
 
